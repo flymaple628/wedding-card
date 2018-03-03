1cf459dc33402f21e28ca824e9034b8ce93cace6
aaron <flymaple628@gmail.com>
2018-03-03 12:28:22 +0800

    change infomation

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,17 +4,17 @@
   <div class="container">
     <div class="row d-block d-sm-none">
       <div class="col-12">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.3836176152236!2d120.47587911462017!3d22.676758985129297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e17771349d9b9%3A0xe2fcc5f62525e3d8!2z5a-M55uI5Zac5a605pyD6aSo!5e0!3m2!1szh-TW!2stw!4v1515910459752" width="350" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14664.987372071753!2d120.34621751885622!3d23.234102933654206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfd9c73d45004735e!2z5rC05p6X6YeM5rS75YuV5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1520050765015" width="350" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
     </div>
     <div class="row d-none d-sm-block ">
       <div class="col-sm-6 offset-sm-3">
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.3836176152236!2d120.47587911462017!3d22.676758985129297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e17771349d9b9%3A0xe2fcc5f62525e3d8!2z5a-M55uI5Zac5a605pyD6aSo!5e0!3m2!1szh-TW!2stw!4v1515910459752" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14664.987372071753!2d120.34621751885622!3d23.234102933654206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfd9c73d45004735e!2z5rC05p6X6YeM5rS75YuV5Lit5b-D!5e0!3m2!1szh-TW!2stw!4v1520050765015" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
     </div>
     <div class="row m-2">
       <div class="col-12  ">
-        <p>地址：屏東縣屏東市和平路422號</p>
+        <p>地址：734台南市六甲區民族街269號</p>
       </div>
     </div>
     <div class="row m-2">
